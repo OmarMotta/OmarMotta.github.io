26e13dc1c9da73ed30a8996de16ff4a58c9be03e
OmarMotta <nicolomotta.gm@gmail.com>
2024-11-25 01:08:06 +0100

    sito

---
+++ script.js
@@ -0,0 +1,6 @@
+// Form submission alert
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Grazie per avermi contattato! Ti risponderò al più presto.");
+});
+
